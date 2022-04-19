422cd71283a067aaf2484443986a64b38fa946e0
ayu <2351753337@qq.com>
2022-04-19 16:57:58 +0800

    添加头部导航栏及基本布局

---
+++ day_01_百度一下/index.js
@@ -0,0 +1,10 @@
+let headerLeftList = [
+    { name: '新闻', href: 'http://news.baidu.com/'},
+    { name: 'hao123', href: 'https://www.hao123.com/?src=from_pc'},
+    { name: '地图', href: 'https://map.baidu.com/'},
+    { name: '贴吧', href: 'https://tieba.baidu.com/index.html'},
+    { name: '视频', href: 'https://haokan.baidu.com/?sfrom=baidu-top'},
+    { name: '图片', href: 'https://image.baidu.com/'},
+    { name: '网盘', href: 'https://pan.baidu.com/?from=1026962h'},
+    { name: '更多', href: 'https://www.baidu.com/more/'},
+]
