1d2f4bd325d64a40f81378a48a8490a60ef40cc1
ayu <2351753337@qq.com>
2022-04-21 17:25:35 +0800

    添加头部导航栏及基本布局

--- day_01_百度一下/index.js
+++ day_01_百度一下/index.js
@@ -8,3 +8,12 @@ let headerLeftList = [
     { name: '网盘', href: 'https://pan.baidu.com/?from=1026962h'},
     { name: '更多', href: 'https://www.baidu.com/more/'},
 ]
+
+let hotList = [
+    { number: 1, text: '上海印发保供物资食安管理意见'},
+    { number: 2, text: '1人感染新冠将持续排毒7到12天'},
+    { number: 3, text: '一文读懂个人养老金制度'},
+    { number: 4, text: '女生花5万买巧克力送校友被网暴'},
+    { number: 5, text: '普京：敌人威胁俄罗斯前要三思了'},
+    { number: 6, text: '小红书被曝整体裁员20%'},
+]
